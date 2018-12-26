764f4cf0293033ff204f2426fa93151ff0fd17ea
johnnycashash <jAG@N1993>
2018-12-26 22:15:07 +0530

    initial

---
+++ src/main/java/org/johnnycash/dummy/DummySpringBootAppApplication.java
@@ -0,0 +1,14 @@
+package org.johnnycash.dummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummySpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummySpringBootAppApplication.class, args);
+	}
+
+}
+
